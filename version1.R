4fae850590909407fad152b540daa818d2c0a8b4
Alejandro Erazo <yerazo3599@gmail.com>
2020-11-01 22:02:11 -0500

    modificadas las tildes

--- version1.R
+++ version1.R
@@ -7,7 +7,7 @@ plot <- ggplot + aes(x= cord, y = puunt)+
         geom_line + theme_void
         
 
-# De ahora en adelante no máss procrastinación,
+# De ahora en adelante no mÃ¡s procrastinaciÃ³n,
 # por ello, estoy haciendo este archivo de prueba
 # para ver si este archivo se modifica junto con el que tengo
 # en visual studio code
