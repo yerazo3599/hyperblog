7ce7f30637438b7b903054c681366fdadbc29460
Alejandro Erazo <yerazo3599@gmail.com>
2020-11-01 20:08:13 -0500

    Ver si la version de R cambio

--- version1.R
+++ version1.R
@@ -6,3 +6,8 @@ data <- import (GINI2010)
 plot <- ggplot + aes(x= cord, y = puunt)+ 
         geom_line + theme_void
         
+
+# De ahora en adelante no máss procrastinación,
+# por ello, estoy haciendo este archivo de prueba
+# para ver si este archivo se modifica junto con el que tengo
+# en visual studio code
