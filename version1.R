5ef2b459db511526a9106880ba2fea03844bcb2e
Alejandro Erazo <yerazo3599@gmail.com>
2020-11-04 01:00:00 -0500

    Función y for

--- version1.R
+++ version1.R
@@ -7,7 +7,13 @@ plot <- ggplot + aes(x= cord, y = puunt)+
         geom_line + theme_void
         
 
-# De ahora en adelante no más procrastinación,
+# De ahora en adelante no m�ss procrastinaci�n,
 # por ello, estoy haciendo este archivo de prueba
 # para ver si este archivo se modifica junto con el que tengo
 # en visual studio code
+function(x,y) {
+        for (i in 3:17) {
+           plot(x, y)
+           print(paste(names(df), sep = " "))
+        }
+}
