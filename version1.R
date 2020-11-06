f5e3a5342544c82ac4b11a7c70321cf9c5658af0
Alejandro Erazo <yerazo3599@gmail.com>
2020-11-05 23:31:25 -0500

    si funciona

--- version1.R
+++ version1.R
@@ -63,5 +63,7 @@ p <- ggplot(iris, mapping = aes(x= Sepal.Width, y=Sepal.Width)) +
 p
 names(iris)
  
- #commit1 
- Prueba para ver si sí sirven los checkout nombre.archivo para volver en el tiempo
+ #commit 1 de checout 
+
+ #comit 2 de checout
+ Cambios después de verificar que el git checout nombre.archivo SÍ FUNCIONA
